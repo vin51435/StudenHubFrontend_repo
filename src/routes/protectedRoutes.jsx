29d3d23c7e5606c9396febbab06400535a9528fc
VIN <v3p51435@gmail.com>
2024-09-11 20:15:18 +0530

    Optimized Routes

--- src/routes/protectedRoutes.jsx
+++ src/routes/protectedRoutes.jsx
@@ -8,30 +8,27 @@ const ProtectedRoutes = () => {
   const { isAuthenticated, token, user } = useSelector((state) => state.auth);
   const { userEdProfile } = useSelector((state) => state.userEdProfile);
   const dispatch = useDispatch();
-
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     if (!isAuthenticated && !user) {
       fetchUserInfo(dispatch).finally(() => setLoading(false));
+    } else {
+      setLoading(false); // Stop loading if user is authenticated or user data is available
     }
   }, [isAuthenticated, token, user, dispatch]);
 
-  console.log({ isAuthenticated }, { userEdProfile });
-
-  if (isAuthenticated) {
-    return (
-      userEdProfile ? <Outlet /> : 'form'
-    );
-  }
+  // console.log({ isAuthenticated }, { userEdProfile });
 
   if (loading) {
     return <PageLoadingSpinner />;
   }
 
-  if (!isAuthenticated) {
-    return <Navigate to='/login' />;
+  if (isAuthenticated) {
+    return userEdProfile ? <Outlet /> : 'form'; // Replace 'form' with the actual component or redirect if needed
   }
+
+  return <Navigate to='/login' />;
 };
 
 export default ProtectedRoutes;
