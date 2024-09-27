757cb4ff5ed80c77f7218dd1299f210aa8a1bd82
VIN <v3p51435@gmail.com>
2024-09-27 18:42:22 +0530

    Updated vite config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import { defineConfig } from 'vite';
 import path from 'path';
 import react from '@vitejs/plugin-react-swc';
 import vitePluginSass from 'vite-plugin-sass';
-import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
